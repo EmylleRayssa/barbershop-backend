2da36b6b7d557e83acd25e3e5d7dc110656cffe8
Jefferson Ramos Lucas dos Anjos <emyllerayssasilva@gmail.com>
2025-03-19 23:31:54 -0300

    change database

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/agendamentos")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/barbershop")
 JWT_SECRET = os.getenv("JWT_SECRET", "meusecretjwt")
 JWT_ALGORITHM = "HS256"
